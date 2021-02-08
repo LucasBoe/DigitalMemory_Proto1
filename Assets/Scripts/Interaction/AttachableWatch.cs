c739e117f24484ff3549f0c6613709c0bc732c04
LucasBoe <lucas@lumotex.de>
2021-02-08 17:02:16 +0100

    arms of watch now animate smoothly

--- Assets/Scripts/Interaction/AttachableWatch.cs
+++ Assets/Scripts/Interaction/AttachableWatch.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class AttachableWatch : SimpleAttachable
 {
     [SerializeField] Transform hourHand, minuteHand;
+    [SerializeField] AnimationCurve timeBlendingCurve;
+
+    float currentTime, targetTime;
+    bool isBlending = false;
+    float blendStartTime, blendEndTime, blendedTime;
+
     private void OnEnable()
     {
-         Game.TimeHandler.OnTimeChanged += OnTimeChanged;
+        Game.TimeHandler.OnTimeChanged += OnTimeChanged;
     }
 
     private void OnDisable()
@@ -17,8 +23,40 @@ public class AttachableWatch : SimpleAttachable
 
     private void OnTimeChanged(int newTime)
     {
+        if (isBlending)
+            currentTime = blendedTime;
+
+        blendStartTime = Time.time;
+        blendEndTime = blendStartTime + timeBlendingCurve.keys[timeBlendingCurve.length - 1].time;
+        targetTime = newTime;
+        isBlending = true;
+    }
+
+    private void Update()
+    {
+        if (isBlending)
+        {
+            float alpha = timeBlendingCurve.Evaluate(Time.time - blendStartTime);
+            blendedTime = Mathf.Lerp(currentTime, targetTime, alpha);
+            SetVisualsForTime(blendedTime);
+
+            if (Time.time >= blendEndTime)
+            {
+                isBlending = false;
+                currentTime = targetTime;
+                SetVisualsForTime(currentTime);
+            }
+        }
+    }
+
+    private void SetVisualsForTime(float blendedTime)
+    {
         Vector3 hourEuler = hourHand.localRotation.eulerAngles;
-        Quaternion newRoration = Quaternion.Euler(hourEuler.x, ((float)(newTime % 12)/12f) * 360f, hourEuler.z);
-        hourHand.localRotation = newRoration;
+        Quaternion hourRoration = Quaternion.Euler(hourEuler.x, ((blendedTime % 12f) / 12f) * 360f, hourEuler.z);
+        hourHand.localRotation = hourRoration;
+
+        Vector3 minuteEuler = minuteHand.localRotation.eulerAngles;
+        Quaternion minuteRoration = Quaternion.Euler(minuteEuler.x, (blendedTime % 1f) * 360f, minuteEuler.z);
+        minuteHand.localRotation = minuteRoration;
     }
 }
