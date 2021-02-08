e35c80feda071f2966fdf2c3ff384e4ef021dbb5
LucasBoe <lucas@lumotex.de>
2021-02-08 14:12:16 +0100

    added randomly pitched sound for rearrangement

--- Assets/Scripts/Interaction/AttachableWatch.cs
+++ Assets/Scripts/Interaction/AttachableWatch.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AttachableWatch : SimpleAttachable
 {
     [SerializeField] Transform hourHand, minuteHand;
+    [SerializeField] AudioClip rearrangeClip;
     private void OnEnable()
     {
          Game.TimeHandler.OnTimeChanged += OnTimeChanged;
@@ -17,6 +18,7 @@ public class AttachableWatch : SimpleAttachable
 
     private void OnTimeChanged(int newTime)
     {
+        Game.SoundPlayer.Play(rearrangeClip,null,volume: 0.25f,randomPitchRange: 0.5f);
         Vector3 hourEuler = hourHand.localRotation.eulerAngles;
         Quaternion newRoration = Quaternion.Euler(hourEuler.x, ((float)(newTime % 12)/12f) * 360f, hourEuler.z);
         hourHand.localRotation = newRoration;
--- Assets/Scripts/Other/SoundPlayer.cs
+++ Assets/Scripts/Other/SoundPlayer.cs
@@ -7,7 +7,7 @@ using System.Linq;
 public class SoundPlayer : Singleton<SoundPlayer>
 {
     List<PlayingAudio> currentlyPlaying = new List<PlayingAudio>();
-    public void Play(AudioClip clip, GameObject toPlayFrom = null)
+    public void Play(AudioClip clip, GameObject toPlayFrom = null, float volume = 1, float randomPitchRange = 0)
     {
         if (clip == null)
             return;
@@ -22,6 +22,8 @@ public class SoundPlayer : Singleton<SoundPlayer>
 
         newAudio.source = gameObject.AddComponent<AudioSource>();
         newAudio.source.clip = clip;
+        newAudio.source.volume = volume;
+        newAudio.source.pitch = 1f + UnityEngine.Random.Range(-randomPitchRange, randomPitchRange);
         newAudio.source.Play();
         newAudio.coroutine = StartCoroutine(WaitForEndOfClipRoutine(newAudio));
         currentlyPlaying.Add(newAudio);
