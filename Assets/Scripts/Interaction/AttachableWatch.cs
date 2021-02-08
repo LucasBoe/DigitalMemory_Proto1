d7c00cfa0039c17e08f1826a3ee72c8eb69ca9b5
LucasBoe <lucas@lumotex.de>
2021-02-08 11:55:49 +0100

    watch is now visually updated based on time

--- Assets/Scripts/Interaction/AttachableWatch.cs
+++ Assets/Scripts/Interaction/AttachableWatch.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AttachableWatch : SimpleAttachable
 {
+    [SerializeField] Transform hourHand, minuteHand;
     private void OnEnable()
     {
          Game.TimeHandler.OnTimeChanged += OnTimeChanged;
@@ -16,6 +17,8 @@ public class AttachableWatch : SimpleAttachable
 
     private void OnTimeChanged(int newTime)
     {
-        //
+        Vector3 hourEuler = hourHand.localRotation.eulerAngles;
+        Quaternion newRoration = Quaternion.Euler(hourEuler.x, ((float)(newTime % 12)/12f) * 360f, hourEuler.z);
+        hourHand.localRotation = newRoration;
     }
 }
