b3321cb68990db8639954cab775fa9a161bf49f9
ollulf <max.bredlau@googlemail.com>
2021-02-08 12:08:42 +0100

    Added Button Animations

--- Assets/Scripts/Time/TimeChangeButton.cs
+++ Assets/Scripts/Time/TimeChangeButton.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class TimeChangeButton : MonoBehaviour, IClickable
 {
     [SerializeField] bool increase;
+    [SerializeField] Animator buttonAnimator;
 
     public void Click()
     {
-        if (increase)
-            Game.TimeHandler.IncreaseTime();
-        else
-            Game.TimeHandler.DecreaseTime();
+        if (buttonAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            if (increase)
+                Game.TimeHandler.IncreaseTime();
+            else
+                Game.TimeHandler.DecreaseTime();
+
+            buttonAnimator.SetTrigger("OnClick");
+        }
+        
     }
 
     public bool IsClickable()
