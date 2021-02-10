8c23f50d8537ad9584a174acd7b2e375f23c40bc
ollulf <max.bredlau@googlemail.com>
2021-02-10 17:39:45 +0100

    Started Shake on Objects

--- Assets/Scripts/Interaction/NonInteractable.cs
+++ Assets/Scripts/Interaction/NonInteractable.cs
@@ -1,19 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class NonInteractable : MonoBehaviour,IClickable
 {
     [SerializeField] AudioClip nonInteractable;
 
+    [SerializeField] float shakeSpeed = 30f, shakeAmount = 0.05f, shakeDuration = 0.5f;
 
     public void Click()
     {
         Game.SoundPlayer.Play(nonInteractable, gameObject);
+        //StartCoroutine(Shake());
+
     }
 
     public bool IsClickable()
     {
         return true;
+    }
+
+    private IEnumerator Shake() {
+
+        float elapsed = 0f;
+        Vector3 myZ = gameObject.transform.position;
+
+        while (elapsed < shakeDuration);
+        {
+            elapsed += Time.deltaTime;
+
+            Vector3 myPosition = gameObject.transform.position;
+            myPosition.z = myZ.z + Mathf.Sin(Time.time * shakeSpeed) * shakeAmount;
+            gameObject.transform.position = myPosition;
+
+            yield return null;
+        }
+
+        gameObject.transform.position = myZ;
+
     }
 }
