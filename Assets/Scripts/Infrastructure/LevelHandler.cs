74a7a81257e464a578d471c13eea98b29b736118
LucasBoe <lucas@lumotex.de>
2021-02-15 13:56:59 +0100

    added loading to level handler (press L and selected the scene you wanna load)

---
+++ Assets/Scripts/Infrastructure/LevelHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelHandler : Singleton<LevelHandler>
+{
+    void OnGUI()
+    {
+        if (Input.GetKey(KeyCode.L))
+        {
+            int i = 0;
+
+            foreach (int index in Game.Settings.playableLevels)
+            {
+                if (GUI.Button(new Rect(10, 10 + 60 * i, 400, 50), "Load: " + SceneManager.GetSceneAt(index).name))
+                {
+                    SceneManager.LoadScene(index, LoadSceneMode.Single);
+                }
+                i++;
+            }
+        }
+    }
+}
