1fa382f53b62bee426f3e87326ef9f093d066819
LucasBoe <lucas@lumotex.de>
2021-02-17 17:44:07 +0100

    fixed weird transitions that max encountered?

--- Assets/Scripts/Time/TimeSetListener.cs
+++ Assets/Scripts/Time/TimeSetListener.cs
@@ -5,36 +5,25 @@ using UnityEngine;
 
 public class TimeSetListener : TimeListener
 {
-    bool listen;
     int lastSet = 0;
     AnimatorStateInfo last;
-    protected override void OnTimeUpdate(float newTime)
-    {
-        if (lastSet != newTime)
-            listen = true;
-
-        base.OnTimeUpdate(newTime);
-    }
 
     private void Update()
     {
-        if (listen)
+        AnimatorStateInfo current = animator.GetCurrentAnimatorStateInfo(0);
+        if (last.shortNameHash != current.shortNameHash)
         {
-            AnimatorStateInfo current = animator.GetCurrentAnimatorStateInfo(0);
-            if (last.shortNameHash != current.shortNameHash)
+            Debug.Log("New State: " + current.shortNameHash);
+            int time = GetTimeFromTagHash(current.tagHash);
+            if (time != int.MinValue)
             {
-                listen = false;
-                int time = GetTimeFromTagHash(current.tagHash);
-                if (time != int.MinValue)
-                {
-                    if ((int)Game.TimeHandler.Time != time)
-                        Game.TimeHandler.ForceTimeSet(time);
-
-                    last = current;
-                }
+                if ((int)Game.TimeHandler.Time != time)
+                    Game.TimeHandler.ForceTimeSet(time);
             }
 
+            last = current;
         }
+
     }
     private int GetTimeFromTagHash(int tagHash)
     {
